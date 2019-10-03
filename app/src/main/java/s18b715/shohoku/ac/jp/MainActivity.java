49a9ab7a7747c36c8a9828fe619d6896bd9c7283
unknown <s18b715@shohoku.ac.jp>
2019-10-03 14:14:52 +0900

    Initial commit

---
+++ app/src/main/java/s18b715/shohoku/ac/jp/MainActivity.java
@@ -0,0 +1,14 @@
+package s18b715.shohoku.ac.jp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
